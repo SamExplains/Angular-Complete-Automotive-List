4ff5f3d008dde8e111ae54324356769110057904
Sampuccino <Sampuccino@gmail.com>
2018-11-01 01:17:16 -0700

    Created a model class to facilitate the creating of new objects of the same type. Added a new static item to the array for presentation view as well as b4 styles to see the layout.

--- src/app/jobs/jobs-list/jobs-list.component.ts
+++ src/app/jobs/jobs-list/jobs-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Job} from '../jobs.model';
 
 @Component({
   selector: 'app-jobs-list',
@@ -6,8 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./jobs-list.component.css']
 })
 export class JobsListComponent implements OnInit {
-
-  constructor() { }
+  jobs: Job[] = [
+    new Job('Ferrari', 'Sports Car', 'https://image1.redbull.com/rbx02014/0010/1/1260/709/528/gallery/images/gallery_rb13_0002_low_34_012_v08_ric.jpg')
+  ];
+  constructor() {}
 
   ngOnInit() {
   }
