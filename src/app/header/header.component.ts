b0a75e42d4f06ba64474344b60fde9a5cf6ee718
Sampuccino <Sampuccino@gmail.com>
2019-01-03 20:44:23 -0800

    Adjusted the routes on the Header/Nav.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -5,9 +5,4 @@ import {Component, EventEmitter, Output} from '@angular/core';
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
-  @Output() featureSelected = new EventEmitter<string>();
-
-  onSelect(nav: string) {
-    this.featureSelected.emit(nav);
-  }
 }
